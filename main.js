e8cf684245c1faff4d9645691abac391d1af3506
JBHutson <hutsonjb@gmail.com>
2019-10-03 20:40:49 -0400

    completed API functionallity

--- main.js
+++ main.js
@@ -5,139 +5,304 @@ class User {
         this.auth = 'Basic ' + btoa(username + ':' + password);
     }
 
-    createUser() {
+    getUsername() {
+        return this.username;
+    }
 
+    getPassword() {
+        return this.password;
+    }
+
+    getAuth() {
+        return this.auth;
+    }
+}
+
+class App {
+    constructor(user) {
+        this.user = user;
+        this.data = {};
+        this.selectedNote = '';
+        this.newUser = false;
+        this.loggedIn = false;
+    }
+
+    renderInitLogin() {
+        if (this.loggedIn == false) {
+            this.revealLoginForm();
+        } else if (this.loggedIn == true && this.newUser == false) {
+            this.hideLoginForm();
+            this.createAndAppendWelcomeChild('Welcome Back!');
+        } else if (this.loggedIn == true && this.newUser == true) {
+            this.hideLoginForm();
+            this.createAndAppendWelcomeChild('New user created!')
+        }
+    }
+
+    renderPage(notes) {
+        this.removeNoteChildren();
+        this.removeLabelAndInputChildren();
+        this.addActionButtonsAndInput();
+        let parent = document.getElementById('notes');
+        let ulChild = document.createElement('ul');
+        ulChild.classList.add('list')
+        ulChild.setAttribute('id', 'list');
+        let self = this;
+        for (let i = 0; i < Object.keys(notes.notes).length; i++) {
+            let child = document.createElement('li');
+            child.classList.add('note');
+            child.addEventListener("click", function() {
+                let clickedNotes = document.querySelectorAll('.note')
+                for (let i = 0; i < clickedNotes.length; i++) {
+                    clickedNotes[i].style.color = 'black';
+                }
+                child.style.color = 'blue';
+                self.selectedNote = notes.notes[i]._id
+                event.preventDefault();
+            })
+            child.innerHTML = '<span> ' + notes.notes[i].text + ' <span>';
+            ulChild.appendChild(child);
+        }
+        parent.appendChild(ulChild);
+    }
+
+    // works
+    loginOrCreateUser() {
         const creationEndpoint = 'https://notes-api.glitch.me/api/users'
+        let self = this;
         fetch(creationEndpoint, {
                 method: 'POST',
-                body: JSON.stringify({ 'username': this.username, 'password': this.password }),
+                body: JSON.stringify({ 'username': this.user.getUsername(), 'password': this.user.getPassword() }),
                 headers: {
                     'Content-Type': 'application/json'
                 }
             })
             .then(function(response) {
-                if (response.status == 422) {
-                    return 'Welcome Back!'
-                } else if (response.status == 201) {
-                    return 'New user successfully created!'
+                return response.status;
+            })
+            .then(function(status) {
+                if (status == 422) {
+                    self.loggedIn = true;
+                    self.renderInitLogin();
+                } else if (status == 201) {
+                    self.loggedIn = true;
+                    self.newUser = true;
+                    self.renderInitLogin();
+                } else {
+                    self.revealInvalidLogin();
+                    self.createAndAppendInvalidLoginChild('Invalid login, please try again.')
                 }
             })
     }
 
-
-
-
-    getAllNotes() {
-
+    // works
+    populateNotes() {
         const allNotesEndpoint = "https://notes-api.glitch.me/api/notes"
+        let self = this;
         fetch(allNotesEndpoint, {
                 method: 'GET',
                 headers: {
-                    'Authorization': this.auth
+                    'Authorization': this.user.getAuth()
                 }
             })
             .then(function(response) {
                 return response.json();
             })
+            .then(function(data) {
+                self.data = data;
+                self.renderPage(data);
+            })
     }
 
-    getNotesByTag(tag) {
+    // works
+    populateNotesByTag(tag) {
         let notesByTagEndpoint = "https://notes-api.glitch.me/api/notes/tagged/" + tag;
+        let self = this;
         fetch(notesByTagEndpoint, {
                 method: 'GET',
                 headers: {
-                    'Authorization': this.auth
+                    'Authorization': this.user.getAuth()
                 }
             })
             .then(function(response) {
-                console.log(response.status);
+                return response.json();
+            })
+            .then(function(data) {
+                self.data = data;
+                self.renderPage(data);
             })
     }
 
+    // works
     createNewNote(text, title, tags) {
         const newNoteEndpoint = "https://notes-api.glitch.me/api/notes"
+        this.self = this;
         fetch(newNoteEndpoint, {
                 method: 'POST',
                 body: JSON.stringify({ 'text': text, 'title': title, 'tags': tags }),
                 headers: {
-                    'Authorization': this.auth,
+                    'Authorization': this.user.getAuth(),
                     'Content-Type': 'application/json'
                 }
             })
             .then(function(response) {
-                console.log(response.status);
+                //
             })
     }
 
-    updateNote(text, title, tags, noteId) {
+    // works
+    deleteNote() {
+        let deleteNoteEndpoint = "https://notes-api.glitch.me/api/notes/" + this.selectedNote;
+        let self = this;
+        fetch(deleteNoteEndpoint, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': this.user.getAuth(),
+                }
+            })
+            .then(function(response) {
+                self.populateNotes();
+            })
+    }
+
+    // works
+    updateNote(noteId, text, title, tags) {
         let updateNoteEndpoint = "https://notes-api.glitch.me/api/notes/" + noteId;
+        let self = this;
         fetch(updateNoteEndpoint, {
                 method: 'PUT',
                 body: JSON.stringify({ 'text': text, 'title': title, 'tags': tags }),
                 headers: {
-                    'Authorization': this.auth,
+                    'Authorization': this.user.getAuth(),
                     'Content-Type': 'application/json'
                 }
             })
             .then(function(response) {
-                console.log(response.status);
+                self.populateNotes();
             })
     }
 
-    deleteNote(noteId) {
-        let deleteNoteEndpoint = "https://notes-api.glitch.me/api/notes/" + noteId;
-        fetch(deleteNoteEndpoint, {
-                method: 'DELETE',
-                headers: {
-                    'Authorization': this.auth
-                }
-            })
-            .then(function(response) {
-                console.log(response.status);
-            })
-    }
-}
-
-class app {
-    constructor(user) {
-        this.user = user;
-        this.loggedIn = false;
+    // works
+    createAndAppendWelcomeChild(message) {
+        let child = document.createElement('p');
+        let parent = document.getElementById('notes');
+        child.innerHTML = '<span> ' + message + ' <span>';
+        child.classList.add('welcome');
+        parent.appendChild(child);
     }
 
-    renderPage() {
-        if (this.loggedIn == false) {
-            //show loggin page
-        } else {
-            //show users main page
+    // works
+    createAndAppendInvalidLoginChild(message) {
+        let child = document.createElement('p');
+        let parent = document.getElementById('invalid-login');
+        child.innerHTML = '<span> ' + message + ' <span>';
+        child.classList.add('error');
+        parent.appendChild(child);
+    }
+
+    addActionButtonsAndInput() {
+        let self = this;
+        let buttParent = document.getElementById('buttons');
+        let inputParent = document.getElementById('input');
+
+        let titleLabel = document.createElement('p');
+        titleLabel.innerHTML = 'Title:';
+        titleLabel.classList.add('label');
+        let title = document.createElement('INPUT');
+        title.classList.add('input');
+        title.setAttribute('id', 'title-field');
+        title.setAttribute('type', 'text');
+
+        let noteTextLabel = document.createElement('p');
+        noteTextLabel.innerHTML = 'Text:';
+        noteTextLabel.classList.add('label');
+        let noteText = document.createElement('textarea');
+        noteText.classList.add('input');
+        noteText.setAttribute('id', 'note-text-field');
+        noteText.setAttribute('type', 'text');
+
+        let noteTagsLabel = document.createElement('p');
+        noteTagsLabel.innerHTML = 'Tags:';
+        noteTagsLabel.classList.add('label');
+        let noteTags = document.createElement('INPUT');
+        noteTags.classList.add('input');
+        noteTags.setAttribute('id', 'note-tags-field');
+        noteTags.setAttribute('type', 'text');
+
+
+        inputParent.appendChild(titleLabel);
+        inputParent.appendChild(title);
+        inputParent.appendChild(noteTextLabel);
+        inputParent.appendChild(noteText);
+        inputParent.appendChild(noteTagsLabel);
+        inputParent.appendChild(noteTags);
+
+        let addButt = document.createElement('button');
+        addButt.classList.add('butt');
+        addButt.addEventListener("click", function() {
+            event.preventDefault()
+        });
+        addButt.innerHTML = 'add Note';
+        let upButt = document.createElement('button');
+        upButt.classList.add('butt');
+        upButt.addEventListener("click", function() {
+            event.preventDefault()
+        });
+        upButt.innerHTML = 'update Note';
+        let delButt = document.createElement('button');
+        delButt.classList.add('butt');
+        delButt.addEventListener('click', function() {
+            self.deleteNote();
+            event.preventDefault()
+        });
+        delButt.innerHTML = 'delete Note';
+        buttParent.appendChild(addButt);
+        buttParent.appendChild(upButt);
+        buttParent.appendChild(delButt);
+    }
+
+    removeNoteChildren() {
+        let parent = document.getElementById('list')
+        if (parent != null) {
+            parent.parentElement.removeChild(parent);
         }
     }
 
-    loginOrCreateUser() {
-        //login or create user
+    removeLabelAndInputChildren() {
+        let inputs = document.querySelectorAll('.input');
+        let parent = document.getElementById('input');
+        for (let i = 0; i < inputs.length; i++) {
+            parent.removeChild(inputs[i]);
+        }
+        let labels = document.querySelectorAll('.label');
+        for (let i = 0; i < labels.length; i++) {
+            parent.removeChild(labels[i]);
+        }
+        let buttons = document.querySelectorAll('.butt');
+        let buttParent = document.getElementById('buttons');
+        for (let i = 0; i < buttons.length; i++) {
+            buttParent.removeChild(buttons[i]);
+        }
     }
 
-
-
-    populateNotes() {
-        // get all notes and render on page
+    hideInvalidLogin() {
+        document.getElementById('invalid-login').classList.add('hidden');
     }
 
-    populateNotesByTag() {
-        // get all notes with a certain tag and populate them
+    revealInvalidLogin() {
+        document.getElementById('invalid-login').classList.add('hidden');
     }
 
-    getCreateNewNoteData() {
-        // get create new note data from form
+    hideLoginForm() {
+        document.getElementById('login-form').classList.add('hidden');
     }
 
-    getDeleteNoteData() {
-        // get delete note data from form
-    }
-
-    getUpdateNoteData() {
-        // get update note data from form
+    revealLoginForm() {
+        document.getElementById('login-form').classList.remove('hidden');
     }
 }
 
 user = new User('james', 'matt');
-user.getAllNotes();
+app = new App(user);
+app.loginOrCreateUser();
+app.populateNotes();
